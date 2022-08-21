76e958d2a29b8e14a3df3c819694d0458a424887
francisco <fu.dev.web@gmail.com>
2022-08-21 19:45:51 +0200

    all items added + product.js created

--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,30 @@
-function request(){
-    fetch("http://localhost:3000/api/products")
-    .then(function(products){
-        if (products.ok){
-            return products.json();
-        };
-    })
-    .then(function(data){
-        console.log(data);
-    })
-    .catch(function(err){
-        console.log('error communication. JSON not received!')
-    })
-};
+const section = document.getElementById('items');
+let urlRequest = 'http://localhost:3000/api/products';
+
+async function request(){
+    const response = await fetch(urlRequest);
+    // console.log(response);
+    const data =  await response.json();
+    console.log(data);
+    data.forEach(element => {
+        const newLink = document.createElement("a");
+        newLink.href = './product.html?id=' + element._id;
+        section.appendChild(newLink);
+        const newArticle = document.createElement("article");
+        newLink.appendChild(newArticle);
+        const newImg = document.createElement("img");
+        newImg.src = element.imageUrl;
+        newImg.alt =  element.altTxt;
+        const newH3 = document.createElement('h3');
+        newH3.className = "productName";
+        newH3.innerText = element.name;
+        const newP = document.createElement('p');
+        newP.className = "productDescription";
+        newP.innerText = element.description;
+        newArticle.appendChild(newImg);
+        newArticle.appendChild(newH3);
+        newArticle.appendChild(newP);
+    });
+}
 
 request();
-
-
